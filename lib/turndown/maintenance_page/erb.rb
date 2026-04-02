f27b4bd91883dc02d1c03beb4afa706bd195d8ed
Guillermo Guerini <thump.crummy7w@icloud.com>
2026-04-02 09:43:59 -0400

    drop: remove i18n and OrderedOptions; modernize Configuration

--- lib/turndown/maintenance_page/erb.rb
+++ lib/turndown/maintenance_page/erb.rb
@@ -2,17 +2,15 @@ require 'erb'
 require 'tilt'
 require 'tilt/erb'
 require_relative './html'
-require_relative '../i18n/internationalization'
 
 module Turndown
   module MaintenancePage
     class Erb < Turndown::MaintenancePage::HTML
 
       def content
-        Turndown::Internationalization.initialize_i18n(@options[:env])
-        Tilt.new(File.expand_path(path)).render(self, {reason: reason}.merge(@options))
+        Tilt.new(File.expand_path(path)).render(self, { reason: reason }.merge(@options))
       end
-      
+
       def self.extension
         'html.erb'
       end
