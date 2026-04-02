0c79d7d4d336cac4c8cee14824aca14675337247
Guillermo Guerini <thump.crummy7w@icloud.com>
2026-04-02 10:17:31 -0400

    chore: simplify Rails engine; require Rails 7+ (drop legacy Rails::Engine guard)

--- lib/turndown/engine.rb
+++ lib/turndown/engine.rb
@@ -4,15 +4,10 @@ require 'turndown'
 require 'rack/turndown'
 require 'rails'
 
-# For Rails 3
-if defined? Rails::Engine
-  module Turndown
-    class Engine < Rails::Engine
-      initializer 'turndown.add_to_middleware_stack' do |app|
-        unless Turndown.config.skip_middleware
-          app.config.middleware.use(Rack::Turndown)
-        end
-      end
+module Turndown
+  class Engine < Rails::Engine
+    initializer 'turndown.add_to_middleware_stack' do |app|
+      app.config.middleware.use(Rack::Turndown) unless Turndown.config.skip_middleware
     end
   end
 end
