08c658f9ac11d54744d3cc16bca0cdc495617aba
Guillermo Guerini <thump.crummy7w@icloud.com>
2026-04-02 09:58:51 -0400

    rack3: replace rack-accept with inline JSON content negotiation

--- lib/turndown/maintenance_page.rb
+++ lib/turndown/maintenance_page.rb
@@ -1,22 +1,43 @@
 # frozen_string_literal: true
 
 require 'pathname'
+
 module Turndown
   module MaintenancePage
-    require 'rack/accept'
-
     def self.all
       @all ||= []
     end
 
+    # Select the best page class for the given Rack env.
+    # JSON pages are served when the Accept header signals JSON and does not include text/html.
+    # Falls back to a custom page if one exists on disk; otherwise uses the configured default.
     def self.best_for(env)
-      request = Rack::Accept::Request.new(env)
+      accept = env['HTTP_ACCEPT'].to_s
 
-      all_types = all.map(&:media_types).flatten
-      best_type = request.best_media_type(all_types)
-      best = all.find { |page| page.media_types.include?(best_type) && File.exist?(page.new.custom_path) }
-      best || Turndown.config.default_maintenance_page
+      if json_request?(accept)
+        custom = all.find { |page| json_page?(page) && File.exist?(page.new.custom_path) }
+        custom || Turndown::MaintenancePage::JSON
+      else
+        custom = all.find { |page| html_page?(page) && File.exist?(page.new.custom_path) }
+        custom || Turndown.config.default_maintenance_page
+      end
     end
+
+    def self.json_request?(accept)
+      Turndown::MaintenancePage::JSON.media_types.any? { |type| accept.include?(type) } &&
+        !accept.include?('text/html')
+    end
+    private_class_method :json_request?
+
+    def self.json_page?(page_class)
+      (page_class.media_types & Turndown::MaintenancePage::JSON.media_types).any?
+    end
+    private_class_method :json_page?
+
+    def self.html_page?(page_class)
+      (page_class.media_types & Turndown::MaintenancePage::HTML.media_types).any?
+    end
+    private_class_method :html_page?
 
     require 'turndown/maintenance_page/base'
     require 'turndown/maintenance_page/erb'
